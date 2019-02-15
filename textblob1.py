58f57a49c2d90703f2da3c4872148aeb928b77c1
saikrishna759 <sai.sanniboina@gmail.com>
2019-02-15 22:00:09 +0530

    only project files
    deleted unneccessary files

--- textblob1.py
+++
@@ -1,7 +0,0 @@
-from textblob import TextBlob
-
-data = TextBlob(u'Hi i am angry on you ,as bad as have a bad opinion on u')
-print(data.tags)
-print(data.words)
-print(data.sentiment.polarity)
-print(data.translate(to = 'te'))

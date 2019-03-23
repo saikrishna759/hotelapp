b3cd69de23a8bd8736f5a5c879e91e7f7919dd5b
saikrishna759 <sai.sanniboina@gmail.com>
2019-03-23 12:39:29 +0530

    grocery+kirana

--- bulkcheck.py
+++
@@ -1,4 +0,0 @@
-import requests
-file = open("E:\\github\\hotelapp\\static\\bulks\\test1\\test2.png","w")
-r = requests.post(url, files=files)
-#print("\\static\\IMAGES\\11.jpg",file=file)
--- testing1.py
+++
@@ -1,3 +0,0 @@
-
-file = open(r"E:\\github\\hotelapp\\static\\bulks\\test1\\test2.jpg","w")
-print("\\static\\IMAGES\\11.jpg",file=file)
